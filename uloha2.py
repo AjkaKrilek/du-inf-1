0a380a07ff0a495aab038114265287030e66e0ce
AjkaKrilek <113473323+AjkaKrilek@users.noreply.github.com>
2022-10-18 21:18:42 +0200

    Add files via upload

---
+++ uloha2.py
@@ -0,0 +1,7 @@
+a=int(input('zadaj mi zaciatok intervalu:'))
+b=int(input('zadaj mi koniec intervalu:'))
+x=int(input('zadaj cislo:'))
+if x>a and x<b:
+    print('patri do intervalu')
+else:
+    print('nepatri do intervalu')
